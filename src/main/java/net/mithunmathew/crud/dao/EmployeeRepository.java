bcef56aa7b0f6135768c41b23d38ed881f8ec306
Mithun Mathew <mithunmathew007@gmail.com>
2021-05-14 11:45:09 +0530

    Added endpoint configuration and pagination

--- src/main/java/net/mithunmathew/crud/dao/EmployeeRepository.java
+++ src/main/java/net/mithunmathew/crud/dao/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package net.mithunmathew.crud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import net.mithunmathew.crud.entity.Employee;
 
+// Spring Data REST creates endpoint as "/employees" by default (adds 's' to Entity). To override it.
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
